44d46d57787a8756a55545fb4c1c64a387189a40
jubayer1418 <skjubayer1418@gmail.com>
2023-05-02 01:25:28 +0600

    data

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const chef = require("./data/chef.json");
+const cors = require("cors");
+app.use(cors());
 app.get("/", (req, res) => {
-  res.send("Hello World!ghjn fdyhfg dfxgdfh");
+  res.send("Hello World!");
+});
+app.get("/chef", (req, res) => {
+  res.send(chef);
+  //   console.log(chef);
+});
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedchef = chef.find((n) => n.id == id);
+  res.send(selectedchef);
 });
 
 app.listen(port, () => {
